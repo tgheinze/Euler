a08f20c565f03a60b8af00048c7b4e4aeff0ee18
Terry Heinze <tgheinze@gmail.com>
2016-12-04 13:29:24 -0600

    updated

--- euler_318_2.py
+++ euler_318_2.py
@@ -3,6 +3,7 @@ __author__ = 'tgheinze'
 from decimal import *
 from math import *
 
+#initialize constants
 s9 = 2011
 
 template = []
@@ -11,58 +12,18 @@ for i in range(0, s9):
 
 s2011 = ''.join(template)
 
-
-# a = Decimal(2)
-# b = Decimal(3)
-# c = Decimal(.5)
-#
-# n = 2018
-#
-#
-#
-# prec = 4000
-# getcontext().prec = prec
-# cont = True
-# while cont:
-#     x = (a**c + b**c)**(Decimal(2*n))
-#     dec = str(x - int(x))[1:2]
-#     if dec != '.':
-#         print('setting prec')
-#         prec += 100
-#         getcontext().prec = prec
-#         continue
-#     num = len(str(int(x)))
-#     getcontext().prec = num + s9 + 10
-#     x = (a ** c + b ** c) ** (Decimal(2 * n))
-#     print(x)
-#     dec = str(x-int(x))[2:]
-#     #print(dec)
-#     i = 0
-#     while True:
-#         if dec[i] == '9':
-#             i += 1
-#             continue
-#         else: break
-#     print(n, '      ', i)
-#     #print(len(dec))
-#     if dec[0:s9] == s2011:
-#         print('n: ', n)
-#         cont = False
-#         break
-#     n += 1
+#7922816251426433759354395033
 
 a = Decimal(2)
-b = Decimal(6)
+b = Decimal(3)
 c = Decimal(.5)
 n = 2000
 reset = 50
-ans1 = 0
 ans = 0
 prec = 4000
 getcontext().prec = prec
-cont = True
-while cont:
-    print(n)
+
+while False:
     x = (a ** c + b ** c) ** (Decimal(2 * n))
     dec = str(x - int(x))[1:2]
     if dec != '.':
@@ -91,10 +52,8 @@ while cont:
         # return
     # print(len(dec))
     if dec[0:s9] == s2011:
-        print('n: ', n, ' for (p,q) = (', p, ',', q, ')')
+        print('n: ', n, ' for (p,q) = (',a, ',',b, ')')
         ans += n
-        if p == 1: ans1 += 1
-        cont = False
         break
     if (2011 - i) > 2:
         n += (2011 - i)
@@ -103,8 +62,60 @@ while cont:
         n += 1
 
 print(ans)
-print(ans1)
 
+
+print('-------------------------------------------')
+
+def getPairs(min):
+
+    ans = []
+
+    for p in range(1, 1006):
+        for q in range(p + 1, 2012 - p):
+            a = Decimal(p)
+            b = Decimal(q)
+            c = Decimal(.5)
+
+            n = min
+
+            prec = 50
+            getcontext().prec = prec
+
+            e = Decimal(2 * n)
+
+            x = (a ** c + b ** c) ** e
+            dec = str(x - int(x))[1:2]
+            num = int(x)
+            #print(str(x))
+            ##print(len(str(num)))
+            if dec != '.':
+                if len(str(num)) < prec:
+                    continue
+                else:
+                    print("error")
+                    return
+
+            #num = len(str(int(x)))
+            dec = str(x - int(x))[2:]
+            #print(dec)
+            i = 0
+            while i < len(dec):
+                if dec[i] == '9':
+                    i += 1
+                    continue
+                else:
+                    break
+            #print(n, '      ', i)
+            if i > 0:
+                #print(a, b, i)
+                ans.append((a,b))
+
+
+    print(ans)
+    return ans
+pairs = getPairs(10)
+print(len(pairs))
+print(pairs)
 print('-------------------------------------------')
 
 
